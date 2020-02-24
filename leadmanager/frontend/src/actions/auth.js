eedd19877382a3806f9ec96591e6d5b02ed28973
kudzanaimadzongwe <kudziemadzongwe6@gmail.com>
2020-02-24 18:09:16 +0200

    commited

--- leadmanager/frontend/src/actions/auth.js
+++ leadmanager/frontend/src/actions/auth.js
@@ -1,12 +1,75 @@
 import axios from "axios";
 
-import { USER_LOADED, USER_LOADING, AUTH_ERROR } from "./types";
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS
+} from "./types";
 
 // CHECK TOKEN & LOAD USER
 export const loadUser = () => (dispatch, getState) => {
   // User Loading
   dispatch({ type: USER_LOADING });
 
+  axios
+    .get("/api/auth/user", tokenConfig(getState))
+    .then(res => {
+      dispatch({
+        type: USER_LOADED,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: AUTH_ERROR
+      });
+    });
+};
+
+// LOGIN USER
+export const login = (username, password) => dispatch => {
+  // Headers
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  // Request Body
+  const body = JSON.stringify({ username, password });
+  axios
+    .post("/api/auth/login", body, config)
+    .then(res => {
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+    })
+    .catch(err => {
+      dispatch({
+        type: LOGIN_FAIL
+      });
+    });
+};
+
+// LOGOUT USER
+export const logout = () => (dispatch, getState) => {
+  axios
+    .post("/api/auth/logout/", null, tokenConfig(getState))
+    .then(res => {
+      dispatch({
+        type: LOGOUT_SUCCESS
+      });
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+// Setup config with token - helper function
+export const tokenConfig = getState => {
   // Get token from state
   const token = getState().auth.token;
 
@@ -22,17 +85,5 @@ export const loadUser = () => (dispatch, getState) => {
     config.headers["Authorization"] = `Token ${token}`;
   }
 
-  axios
-    .get("/api/auth/user", config)
-    .then(res => {
-      dispatch({
-        type: USER_LOADING,
-        payload: res.data
-      });
-    })
-    .catch(err => {
-      dispatch({
-        type: AUTH_ERROR
-      });
-    });
+  return config;
 };
